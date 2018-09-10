2ae91fc513e093b578c7ca86807285e0f7559512
bilalkocoglu <bilal.kocoglu@outlook.com.tr>
2018-09-11 00:53:18 +0300

    thy controller

--- src/main/java/com/example/youtube/stockservice/controller/ThyController.java
+++ src/main/java/com/example/youtube/stockservice/controller/ThyController.java
@@ -1,6 +1,8 @@
 package com.example.youtube.stockservice.controller;
 
+import com.example.youtube.stockservice.model.User;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +13,11 @@ public class ThyController {
     static final String END_POINT = "/thy";
 
     @GetMapping(value = "/deneme", produces = MediaType.APPLICATION_JSON_VALUE)
-    public String deneme(){
-        return "THY deneme";
+    public ResponseEntity deneme(){
+        User user = new User();
+        user.setUserId(2);
+        user.setName("THY");
+        user.setPassword("API");
+        return ResponseEntity.ok().body(user);
     }
 }
