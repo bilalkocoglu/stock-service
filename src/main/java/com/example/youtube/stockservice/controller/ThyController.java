89681e7585efc36a26a8f3253aa342367590d0c8
bilalkocoglu <bilal.kocoglu@outlook.com.tr>
2018-09-11 00:33:23 +0300

    thy controller deneme endpoint

--- src/main/java/com/example/youtube/stockservice/controller/ThyController.java
+++ src/main/java/com/example/youtube/stockservice/controller/ThyController.java
@@ -1,12 +1,22 @@
 package com.example.youtube.stockservice.controller;
 
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.logging.LogManager;
+import java.util.logging.Logger;
 
 @RestController
 @RequestMapping(value = this.END_POINT)
 public class ThyController {
     static final String END_POINT = "/thy";
 
+    private static final Logger log = (Logger) org.apache.logging.log4j.LogManager.getLogger(UserController.class);
 
+    @GetMapping(value = "/deneme", produces = MediaType.APPLICATION_JSON_VALUE)
+    public String deneme(){
+        return "THY deneme";
+    }
 }
